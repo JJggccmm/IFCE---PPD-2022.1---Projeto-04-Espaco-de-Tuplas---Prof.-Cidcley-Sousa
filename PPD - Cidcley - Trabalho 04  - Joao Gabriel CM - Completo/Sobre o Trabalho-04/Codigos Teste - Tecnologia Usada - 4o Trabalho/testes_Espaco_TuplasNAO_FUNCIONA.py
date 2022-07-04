707551e8067dd0b24b6c6b038496b52c82b941c8
João GabCM(2019-PO) <38107176+Numbess@users.noreply.github.com>
2022-07-04 15:49:26 -0300

    Add files via upload

---
+++ PPD - Cidcley - Trabalho 04  - Joao Gabriel CM - Completo/Sobre o Trabalho-04/Codigos Teste - Tecnologia Usada - 4o Trabalho/testes_Espaco_TuplasNAO_FUNCIONA.py
@@ -0,0 +1,9 @@
+# teste
+
+from lindypy.lindypy import *
+
+ts = tuplespace()
+ts.out(('sum', 0))
+
+
+ts.shutdown()
---
+++ PPD - Cidcley - Trabalho 04  - Joao Gabriel CM - Completo/Sobre o Trabalho-04/lindypy/lindypy/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.2'
+
+from lindypy.lindypy.TupleSpace import tuplespace
+from lindypy.lindypy.Utils import timeout, TimeoutError
